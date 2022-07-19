c1dfadf36ea4ede8510a7f0646940a7f26b9acdb
Vemula Vinay <vinay.vr@prodapt.com>
2022-07-19 09:56:58 +0530

    source code

---
+++ SchoolRecords.java
@@ -0,0 +1,15 @@
+package vinay;
+
+public class SchoolRecords {
+	int schoolId;
+	String schoolName;
+	String schoolAddress;
+	void addRecords(int schoolId, String schoolName, String schoolAddress) {
+	System.out.println(schoolId + " " + " " + schoolName + " " + schoolAddress);
+	}
+	void displayRecords() {
+	System.out.println("Records of Schools in Chennai");
+	}
+
+}
+
